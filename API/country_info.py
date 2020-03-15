796fbd24664a793cb32d640218fb17c8047782b0
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-03-15 19:45:00 +0200

    country info class / connection to the public api

---
+++ API/country_info.py
@@ -0,0 +1,20 @@
+import requests
+
+class CountryInfo:
+
+    def get_info(self, country_name):
+
+        # defining our core url
+        url = 'https://restcountries.eu/rest/v2/name/{name}'
+
+        # concatenating the given country name to the url
+        url = url.format(name=country_name)
+
+        try:
+        # return the required info ftom the url
+            response = requests.get(url).json()
+        # checking fot requests exceptions
+        except requests.RequestException:
+            return None
+
+        return response
