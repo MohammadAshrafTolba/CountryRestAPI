4c4e597df22a57f8116250150976ca90ba9efee7
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-03-15 19:42:16 +0200

    API entry point

---
+++ API/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api, reqparse
+from data_handler import DataHandler
+
+app = Flask(__name__)
+api = Api(app)
+
+# routing path to the DataHandler class
+api.add_resource(DataHandler, '/country/name/<string:name>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
