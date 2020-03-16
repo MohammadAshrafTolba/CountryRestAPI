03ed4db5bd73e5e4cdc90126f522b34a3c7a6dc8
Mohammad Ashraf <g.mohammad.ashraf.99@gmail.com>
2020-03-17 00:21:36 +0200

    data handler is working, needs proper error messages/polishing

--- API/data_handler.py
+++ API/data_handler.py
@@ -1,9 +1,15 @@
 from flask_restful import Resource
+from flask import jsonify
+from country_info import CountryInfo
 
 class DataHandler(Resource):
 
     def __init__(self):
+        self.info = CountryInfo()
 
-        # logic goes here
 
-        pass
+    def get(self, name):
+        returned_info = self.info.get_info(name)
+        if len(returned_info) == 0:
+            return 'Invalid resources'
+        return jsonify(returned_info)
--- API/server.py
+++ API/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 api = Api(app)
 
 # routing path to the DataHandler class
-api.add_resource(DataHandler, '/country/name/<string:name>')
+api.add_resource(DataHandler, '//country/name/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
